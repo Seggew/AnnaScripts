06ea3d1a6aa2eceabeb3804928b774f62f3d52c8
“Anna <anna.seggewisse@gmx.net>
2024-10-22 10:39:30 +0100

    initial commit

---
+++ examples/seaborn_example.py
@@ -0,0 +1,20 @@
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from natsort import natsorted
+
+# Load the master CSV file
+master_csv_path = '/Users/seggewa/Desktop/Density/ClusterInfo_20241022.csv'
+data = pd.read_csv(master_csv_path)
+
+# natural sorting (prevent 100, 150, 50 situations)
+data_sorted = data.loc[natsorted(data.index, key=lambda x: data['Condition'][x])] 
+# %%
+
+fig, ax = plt.subplots(1,1, figsize=(5,4))
+sns.barplot(x=data_sorted.Condition, y=data_sorted.Clusters, errorbar='sd', color='#B8CCC3', ax=ax)
+plt.xticks(rotation=45, ha='right')
+
+# %%
