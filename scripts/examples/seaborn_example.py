6acfe35b1a41150d4f2ad50d3ec3944864135dc0
“Anna <anna.seggewisse@gmx.net>
2024-10-22 11:31:05 +0100

    fixed bug with plot save

--- scripts/examples/seaborn_example.py
+++ scripts/examples/seaborn_example.py
@@ -11,13 +11,14 @@ data = pd.read_csv(master_csv_path)
 
 # natural sorting (prevent 100, 150, 50 situations)
 data_sorted = data.loc[natsorted(data.index, key=lambda x: data['Condition'][x])] 
+
 # %%
 # plotting the plot
 fig, ax = plt.subplots(1,1, figsize=(5,4))
 sns.barplot(x=data_sorted.Condition, y=data_sorted.Clusters, errorbar='sd', color='#B8CCC3', ax=ax)
 plt.xticks(rotation=45, ha='right')
 
-# %%
-# saving the plot
+# saving the plot, make sure it is in the same cell
+plt.savefig('plots/save.png', bbox_inches='tight', dpi=300)
 
-plt.savefig('plot/save.png')
+# %%
