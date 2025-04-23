f0ee6660681112a1cc1259838fa3c65d62245cc4
Anna Seggewisse <anna.seggewisse@gmx.net>
2025-04-23 16:15:27 +0200

    reduce to one origin file only

--- ReduceDataSet.py
+++ ReduceDataSet.py
@@ -2,8 +2,10 @@ import pandas as pd
 
 data = pd.read_csv("combined_clustersDBSCAN.csv")
 
+first_origin_file = data['origin_file'].unique()[0]
+dataOrigin = data[data['origin_file'] == first_origin_file]
 
-data_filtered = data[(data['frame'] >= 25000) & (data['frame'] <= 150000)]
+data_filtered = dataOrigin[(dataOrigin['frame'] >= 25000) & (data['frame'] <= 150000)]
 
 data_toy = data_filtered[data_filtered['frame'] % 100 == 0]
 
